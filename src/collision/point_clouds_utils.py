2c81793118254681f024751028b06b8a2217a498
Jean-Baptiste Weibel <jb.weibel@gmail.com>
2024-06-04 18:39:41 +0200

    line_plane intersection

--- src/collision/point_clouds_utils.py
+++ src/collision/point_clouds_utils.py
@@ -11,3 +11,17 @@ def project_point_cloud(cloud, intrinsics, reference_height, reference_width):
     depth_map[vs, us] = cloud_pts[:, 2]
 
     return depth_map
+
+
+def plane_line_intersection(rays, plane_normal, plane_pt):
+    # Choose the ray that has the smallest dot product
+    # to the plane normal (signed)
+    min_dot_prod_to_plane_idx = np.argmin(
+        np.dot((rays - plane_pt), plane_normal))
+    ray = rays[min_dot_prod_to_plane_idx]
+    ray /= np.linalg.norm(ray)
+
+    # Compute the corresponding intersection between plane and ray
+    t = np.dot(plane_normal, plane_pt) / np.dot(plane_normal, ray)
+
+    return t*ray
